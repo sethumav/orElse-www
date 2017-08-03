2bbcafb09041401bdeb7d8f91227feffca85bd3a
Ming Zhu <Ming_Zhu@wsib.on.ca>
2017-08-03 13:47:14 -0400

    Fixed review page problem.

--- src/app/review/review.component.ts
+++ src/app/review/review.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
 import { Router } from '@angular/router';
 import { MobListService, MobData } from '../service/mob-list.service';
 import { DataSource } from '@angular/cdk';
@@ -7,19 +7,29 @@ import { DataSource } from '@angular/cdk';
     templateUrl: './review.component.html',
     styleUrls: ['./review.component.css']
 })
-export class ReviewComponent implements OnInit {
+export class ReviewComponent implements AfterViewChecked {
+    private tableUpdated = false;
     private mobListService: MobListService;
     private dataSource: DataSource<MobData>;
     displayedColumns = ['section', 'task', 'application', 'respPerson'];
 
     constructor(mobListService: MobListService) {
         this.mobListService = mobListService;
+        this.dataSource = this.mobListService.mbDataSource;
         console.log(this.dataSource);
     }
-    ngOnInit() {
-        this.dataSource = this.mobListService.mbDataSource;
+    ngAfterViewChecked() {
+        // datatable won't load if the data are not change so we wait half seconds to call an update 
+        // will find a better way later
+        setTimeout(this.update, 100);
     }
     update() {
-        this.mobListService.upate();
+        if (this.tableUpdated === false) {
+            this.tableUpdated = true;
+            this.mobListService.upate();
+        }
+    }
+    sendEmails() {
+        
     }
 }
