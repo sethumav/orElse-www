fa23726e0919009711e433b4a2e3575b6d4aace5
kevinchiuwsib <kevin_chiu@wsib.on.ca>
2017-08-15 12:04:43 -0400

    moved vars to top, added check for submit text on submit button

--- todo-spec.js
+++ todo-spec.js
@@ -1,16 +1,20 @@
 describe('orelse front end up and running', function() {
+  var inputExists = element(by.xpath('//*[@id="md-input-1"]')); 
+  var submitExists = element(by.xpath('/html/body/app-root/div/ng-component/div[5]/div/div[2]/button/span'));
+  
   it('should be up and running', function() {
     browser.get('http://localhost:4200');
   });
   
   it('should have an input field somewhere', function(){
-    var inputExists = element(by.xpath('//*[@id="md-input-1"]'));  
     expect(inputExists.isPresent()).toBe(true);
   });
   
   it('should have a submit button', function(){
-    var submitExists = element(by.xpath('/html/body/app-root/div/ng-component/div[5]/div/div[2]/button/span'));
-    expect(submitExists.isPresent()).toBe(true);
+    if (expect(submitExists.isPresent()).toBe(true)){
+      expect(submitExists.getText()).toBe('Submit');
+    }
+    
   });
  
 });
