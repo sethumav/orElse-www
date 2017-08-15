18051717b9322e78eda807443e9c7223139018ad
kevinchiuwsib <kevin_chiu@wsib.on.ca>
2017-08-15 10:36:07 -0400

    adding Protractor spec for input and submit to exist

--- todo-spec.js
+++ todo-spec.js
@@ -2,6 +2,16 @@ describe('orelse front end up and running', function() {
   it('should be up and running', function() {
     browser.get('http://localhost:4200');
   });
+  
+  it('should have an input field somewhere', function(){
+    var inputExists = element(by.xpath('//*[@id="md-input-1"]'));  
+    expect(inputExists.isPresent()).toBe(true);
+  });
+  
+  it('should have a submit button', function(){
+    var submitExists = element(by.xpath('/html/body/app-root/div/ng-component/div[5]/div/div[2]/button/span'));
+    expect(submitExists.isPresent()).toBe(true);
+  });
+ 
 });
-
 
