b9986bc1c84ef4aa76554753fdf2956d932860a7
Vijay Sethumadavan <sethumadavan@gmail.com>
2017-10-05 12:12:59 -0400

    handle empty subject

--- src/app/service/email.service.spec.ts
+++ src/app/service/email.service.spec.ts
@@ -1,6 +1,6 @@
 import { inject, getTestBed, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { EmailService } from '../service/email.service';
+import { EmailService, EmailRequest } from '../service/email.service';
 import { MobData } from '../service/mob-list.service';
 
 import { Injectable, ReflectiveInjector } from '@angular/core';
@@ -9,6 +9,7 @@ import { BaseRequestOptions, ConnectionBackend, Http, RequestOptions, HttpModule
 import { Response, ResponseOptions } from '@angular/http';
 import { MockBackend, MockConnection } from '@angular/http/testing';
 import { Observable  } from 'rxjs/Observable';
+import { ResponsiblePerson } from './mob-list.service';
 
 
 describe('when sendemail', () => {
@@ -40,7 +41,7 @@ describe('when sendemail', () => {
         // expect(service.http.get).toHaveBeenCalled();
     })));
 
-    it('Responose is fetched correctly', async(inject(
+    it('Response is fetched correctly', async(inject(
         [EmailService, MockBackend], (service, mockBackend) => {
         const fakeEmailSendStatusData = [true, false, true];
         const options = new ResponseOptions({status: 200, body: {data: fakeEmailSendStatusData}});
@@ -62,6 +63,32 @@ describe('when sendemail', () => {
         service.sendEmails(new MobData()).then(status => {
                 expect(status).not.toContain(fakeEmailSendStatusCode,
                   'should have valid status code');
+              });
+    })));
+
+    it('Response should be true even if subject is empty', async(inject(
+        [EmailService, MockBackend], (service, mockBackend) => {
+        const fakeEmailSendStatus = true;
+        const fromRespPerson = new ResponsiblePerson("Joe", "joe@test.com");
+        const emailNameAddress = new ResponsiblePerson("Jane", "jane@test.com");
+        const emailNameAddresses = new Array<ResponsiblePerson>();
+        emailNameAddresses.push(emailNameAddress);     
+        const emailSubject = "";
+        const emailBody = "Test Email Body";
+        const emailRequest = new EmailRequest( fromRespPerson,
+                                               emailSubject,
+                                               emailBody,
+                                               emailNameAddresses
+                                             );       
+        const options = new ResponseOptions({status: 200, body: {data: fakeEmailSendStatus}});
+        const response = new Response(options);
+
+        service.emailRequest=emailRequest;
+        service.loadEmailTemplate();
+        mockBackend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
+        service.sendEmails(new MobData()).then(status => {
+                expect(status).toBe(fakeEmailSendStatus,
+                  'should have expected status');
               });
     })));
    
